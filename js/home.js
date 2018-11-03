107789a66ba7eb1904e2e736cca2d9d7428b3a45
liu <2304450839@qq.com>
2018-11-03 16:13:52 +0800

    webpack练习demo

---
+++ js/home.js
@@ -0,0 +1,7 @@
+// var base = require('./base.js')
+// var open = base.open;
+import {open} from './base.js'
+import '../css/base.css';
+if(open) {
+	 console.log("可以注册")
+}
---
+++ js/signup.js
@@ -0,0 +1,6 @@
+import {open}  from './base.js'
+if(open) {
+	console.log("欢迎入坑")
+}else {
+	console.log("暂不接受入坑")
+}
